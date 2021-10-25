6580d7f5ed5352f391d87f7fc90abefa9f5e8243
noni227 <81325180+Noni2207@users.noreply.github.com>
2021-10-25 20:14:58 +0200

    first commit after building

---
+++ src/data.js
@@ -0,0 +1,180 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+    return [
+    {
+      name: "Home Court",
+      artist: "Blue Wednesday, Shopan",
+      cover: "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#B7A9DA","#9F699B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233"
+    },
+    {
+      name: "Murmuration",
+      artist: "Blue Wednesday, Shopan",
+      cover: "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#B7A9DA","#9F699B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7814"
+    },
+    {
+      name: "bringmesun Pt.2",
+      artist: "Psalm Trees, Guillaume Muschalle",
+      cover: "https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/602c02e9e1621825dbf9d186_PsalmTrees-WMBIS-SquareFinal%201.png",
+      id: uuidv4(),
+      active: false,
+      color: ["#F2D04E","#142927"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10538"
+    },
+    {
+      name: "She Won't Say",
+      artist: "Psalm Trees, Guillaume Muschalle",
+      cover: "https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/602c02e9e1621825dbf9d186_PsalmTrees-WMBIS-SquareFinal%201.png",
+      id: uuidv4(),
+      active: false,
+      color: ["#F2D04E","#142927"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10536"
+    },
+    {
+      name: "I Won't Wait for You",
+      artist: "Psalm Trees, Guillaume Muschalle",
+      cover: "https://uploads-ssl.webflow.com/5fc4bca9bd65e8f2bf87f576/602c02e9e1621825dbf9d186_PsalmTrees-WMBIS-SquareFinal%201.png",
+      id: uuidv4(),
+      active: false,
+      color: ["#F2D04E","#142927"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10545"
+    },
+    {
+      name: "Doin It",
+      artist: "Tesk",
+      cover: "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FECA76", "#54291E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8927"
+    },
+    {
+      name: "Today Feels Like Everyday",
+      artist: "Mama Aiuto",
+      cover: "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FECA76", "#54291E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12128"
+    },
+    {
+      name: "i wish it would never stop snow",
+      artist: "Sleepy Fish",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FDB17F", "#008284"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7900"
+    },
+    {
+      name: "Birds",
+      artist: "Harris Cole",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FDB17F", "#008284"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7905"
+    },
+    {
+      name: "Ocean View",
+      artist: "G Mills, Kyle McEvoy, Luke Otwell",
+      cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#D4ABC1", "#88799B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10320"
+    },
+    {
+      name: "Frozen in Time",
+      artist: "Kendall Miles",
+      cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#D4ABC1", "#88799B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10329"
+    },
+    {
+      name: "Daylight",
+      artist: "Aiguille",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#F8C7B8", "#6F5FAA"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272"
+    }, 
+    {
+      name: "Daydream",
+      artist: "Aiguille",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#F8C7B8", "#6F5FAA"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9275"
+    },
+    {
+      name: "Longing",
+      artist: "Makzo",
+      cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E9856E", "#38508E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11777"
+    },
+    {
+      name: "Departure",
+      artist: "Makzo",
+      cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E9856E", "#38508E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11771"
+    },
+    {
+      name: "Sugarless",
+      artist: "The Field Tapes, Aviino",
+      cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#DBE8DF", "#2A2C48"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11243"
+    },
+    {
+      name: "Flowers",
+      artist: "The Field Tapes, Aviino",
+      cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#DBE8DF", "#2A2C48"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11249"
+    },
+    {
+      name: "Jazz Cabbage",
+      artist: "Ian Ewing, Strehlow",
+      cover: "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#F9DDD2", "#573249"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9363"
+    },
+    {
+      name: "Sails",
+      artist: "Strehlow, Aylior",
+      cover: "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#F9DDD2", "#573249"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9355"
+    },
+      
+    ]
+}
+
+export default chillHop
